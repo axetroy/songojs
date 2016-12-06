c491c3242d0684d7d24f5174ec896d32cbf3cc65
axetroy <axetroy@suboat.com>
2016-12-06 17:40:58 +0800

    docs: add index.d.ts file

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,55 @@
-declare function songo(any:any);
+interface Meta {
+  limit?: number,
+  page?: number,
+  skip?: number
+}
+
+interface Entity {
+  query?: any,
+  sort?: string[],
+  meta?: Meta
+}
+
+interface Params {
+  limit: number,
+  page: number,
+  skip?: number,
+  sort?: string,
+  query?: string
+}
+
+declare function songo(entity?: Entity);
+
+declare class Songo {
+  new(entity: Entity): Songo;
+
+  get string(): string;
+
+  toString(): string;
+
+  toQuery(): string;
+
+  toParams(): Params;
+
+  fromParams(paramsObject: Params): Songo;
+
+  clearSort(): Songo;
+
+  indexSort(sortKey: string): number;
+
+  onlySort(sortKey: string): Songo;
+
+  setSort(sortKey: string): Songo;
+
+  unshiftSort(sortKey: string): Songo;
+
+  removeSort(sortKey: string): Songo;
+
+  pushSort(sortKey: string): Songo;
+
+  popSort(): Songo;
+
+  shiftSort(): Songo;
+}
+
+export = songo;
